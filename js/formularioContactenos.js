89d45d4c238f12a9686777614346e3f54506a48f
Josué Gabuardi Bautista <jgabuardib449@ulacit.ed.cr>
2017-12-20 14:47:53 -0600

    Vista contactenos mejorada
    corregi varias errores visuales que tenia contactenos y además implemente el form de boostrap.

--- js/formularioContactenos.js
+++ js/formularioContactenos.js
@@ -1,15 +1,17 @@
 function compruebaEmail(){
-  var email = document.getElementById("exampleInputEmail1").value;
+  var email = document.getElementById("exampleInputEmail").value;
   expr = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
       if ( !expr.test(email) ){
         alert("Introduzca un correo valido");
       }
 }
+
 function EncuentraEmail(){
-  var email = document.getElementById("exampleInputEmail1").value;
+  var email = document.getElementById("exampleInputEmail").value;
   expr = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
       if ( !expr.test(email) ){
       alert("Introduzca un correo valido");
+
       return false;
     }else{
       return true;
@@ -18,10 +20,9 @@ function EncuentraEmail(){
 }
 
 function compruebaEspacios(){
-var Nombre= document.getElementById("Name").value;
-var Correo= document.getElementById("exampleInputEmail1").value;
-var Asunto= document.getElementById("Asunto").value;
-var Mensaje= document.getElementById("Mensaje").value;
+var Nombre= document.getElementById("inputDefault").value;
+var Correo= document.getElementById("exampleInputEmail").value;
+var Mensaje= document.getElementById("exampleTextarea").value;
 var HayEspacioVacio= false;
   document.getElementById("LleneEspacios").innerHTML="";
   if(Nombre==""){
@@ -32,24 +33,20 @@ var HayEspacioVacio= false;
     HayEspacioVacio= true;
   } if(Correo!="" && !EncuentraEmail()){
     HayEspacioVacio= true;
-  } if(Asunto==""){
-    document.getElementById("LleneEspacios").innerHTML+="* Llene el Asunto <br>";
-    HayEspacioVacio= true;
-  }if(Mensaje==""){
+  } if(Mensaje==""){
     document.getElementById("LleneEspacios").innerHTML+="* Llene el Mensaje <br>";
     HayEspacioVacio= true;
   }
 if(HayEspacioVacio==false){
-email();
+  email();
 }
 }
 
 function email(){
- var Nombre= document.getElementById("Name").value;
- var Correo= document.getElementById("exampleInputEmail1").value;
- var Asunto= document.getElementById("Asunto").value;
- var Mensaje= document.getElementById("Mensaje").value;
+ var Nombre= document.getElementById("inputDefault").value;
+ var Correo= document.getElementById("exampleInputEmail").value;
+ var Mensaje= document.getElementById("exampleTextarea").value;
   Mensaje= "Nombre: "+Nombre+"\r\n"+Mensaje;
   Mensaje = encodeURIComponent(Mensaje);
- window.location='mailto:'+Correo+'?subject=' + Asunto + '&body='+Mensaje;
+ window.location='mailto: EduWeb@gmail.com?subject=mensajeConsultaEduWeb' + '&body='+Mensaje;
 }
